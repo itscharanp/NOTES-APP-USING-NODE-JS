8904ec32bf3ff950c56e9b7e79440652866d0778
Pyla Charan kumar <40740755+itscharanp@users.noreply.github.com>
2020-05-01 20:53:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(validator.isEmail('its.charanp@gmail.com'))
 chalk = require('chalk')
 console.log(chalk.blue('HELLO'))
 
-// IMPORT YOUR OWN FILES
+// IMPORT YOUR OWN FILES(app.js)
 const getNotes = require('./notes')
 const mssg = getNotes()
 console.log(mssg)
+
+//CREATE THE FILE(notes.js)
+const getNotes = function()
+{
+    return "Your notes.."
+}
+module.exports = getNotes
